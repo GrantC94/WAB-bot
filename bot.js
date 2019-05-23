24fcd54f2ee611dac939010b9136b300f563dbcb
Grant Coles <grantwc94@gmail.com>
2019-05-23 18:30:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /(WAB)/i;
   console.log(request.name)
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text) && request.name != "WAB Bot") {
     sleep(2000)
     this.res.writeHead(200);
     postMessage();
